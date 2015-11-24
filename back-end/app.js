f2e802136d4f1692326b83bf562199b1038d3159
benlayer291 <benlayer291@gmail.com>
2015-11-24 11:50:40 +0000

    Altered index.html and wrote javascript for ajax registration form

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,5 @@
 var express        = require('express');
+var cors           = require('cors');
 var path           = require('path');
 var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
@@ -22,6 +23,7 @@ require('./config/passport')(passport);
 // Middleware
 app.use(cookieParser());
 app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride(function(req, res){
