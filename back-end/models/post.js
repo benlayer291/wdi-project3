74e84e3023daa24dc201fa701fb804fb15121a7c
benlayer291 <benlayer291@gmail.com>
2015-11-25 16:51:03 +0000

    Created 5 rest routes for requests controller, added requests routes to routes.js and made requests model

--- back-end/models/post.js
+++ back-end/models/post.js
@@ -1,11 +1,12 @@
 var mongoose   = require('mongoose');
 
 var postSchema = new mongoose.Schema({
-  _creator: { type: String, ref: 'User' },
+  // _creator: { type: String, ref: 'User' },
   where: {type: String, required: true},
   when: {type: String, required: true},
   what: {type: String, required: true},
-  status: {type: String, default: 'pending'}
+  status: {type: String, default: 'pending'},
+  requests: [{ type: mongoose.Schema.ObjectId, ref: 'Request' }]
 })
 
 module.exports = mongoose.model('Post', postSchema);
---
+++ back-end/models/request.js
@@ -0,0 +1,9 @@
+var mongoose   = require('mongoose');
+
+var requestSchema = new mongoose.Schema({
+  requester_id: {type: String, required: true},
+  receiver_id: {type: String, required: true},
+  status: {type: String, default: 'pending'}
+})
+
+module.exports = mongoose.model('Request', requestSchema);
