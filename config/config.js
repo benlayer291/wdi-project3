2457b0afc54603984feb015c46490320daa7e7e0
benlayer291 <benlayer291@gmail.com>
2016-01-08 17:30:51 +0000

    Changed config file to aid connection to database

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   'secret': 'jsonwebtokensaregreat',
-  'database': process.env.SPOKEN_MONGOLAB_URI || 'mongodb://localhost:27017/spoken'
+  database: process.env.SPOKEN_MONGOLAB_URI
+  // 'database': process.env.SPOKEN_MONGOLAB_URI || 'mongodb://localhost:27017/spoken'
 };
