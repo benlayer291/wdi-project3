d5ff41f1227fa1c108959373d0c4c1f7fa4d65bb
benlayer291 <benlayer291@gmail.com>
2016-01-08 17:58:17 +0000

    Added spoken database as heroku config variable

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
   'secret': 'jsonwebtokensaregreat',
-  'database': process.env.MONGOLAB_URI || 'mongodb://localhost:27017/spoken'
+  'database': process.env.MONGOLAB_URI_SPOKEN || 'mongodb://localhost:27017/spoken'
 };
